b3cbc31e5afb22818b01e293faf02e0cb2d194a5
Venkat, Ajay [External] <Ajay.Venkat@externals.adidas.com>
2023-04-12 20:56:33 +1000

    Init

---
+++ Assets/GridParticle.shader
@@ -0,0 +1,70 @@
+Shader "Instanced/GridTestParticleShader" {
+	Properties{
+		_MainTex("Albedo (RGB)", 2D) = "white" {}
+		_Glossiness("Smoothness", Range(0,1)) = 0.5
+		_Metallic("Metallic", Range(0,1)) = 0.0
+        _Color("Color", Color) = (0.25, 0.5, 0.5, 1)
+	}
+		SubShader{
+			Tags { "RenderType" = "Opaque" }
+			LOD 200
+
+			CGPROGRAM
+			// Physically based Standard lighting model
+			#pragma surface surf Standard addshadow fullforwardshadows
+			#pragma multi_compile_instancing
+			#pragma instancing_options procedural:setup
+
+			sampler2D _MainTex;
+			float _size;
+            float3 _Color;
+
+			struct Input {
+				float2 uv_MainTex;
+			};
+
+			struct Particle
+			{
+                float pressure;
+                float density;
+                float3 currentForce;
+                float3 velocity;
+				float3 position;
+				
+			};
+
+		#ifdef UNITY_PROCEDURAL_INSTANCING_ENABLED
+			StructuredBuffer<Particle> _particlesBuffer;
+		#endif
+
+			void setup()
+			{
+			#ifdef UNITY_PROCEDURAL_INSTANCING_ENABLED
+				float3 pos = _particlesBuffer[unity_InstanceID].position;
+				float size = _size;
+
+				unity_ObjectToWorld._11_21_31_41 = float4(size, 0, 0, 0);
+				unity_ObjectToWorld._12_22_32_42 = float4(0, size, 0, 0);
+				unity_ObjectToWorld._13_23_33_43 = float4(0, 0, size, 0);
+				unity_ObjectToWorld._14_24_34_44 = float4(pos.xyz, 1);
+				unity_WorldToObject = unity_ObjectToWorld;
+				unity_WorldToObject._14_24_34 *= -1;
+				unity_WorldToObject._11_22_33 = 1.0f / unity_WorldToObject._11_22_33;
+			#endif
+			}
+
+			half _Glossiness;
+			half _Metallic;
+
+			void surf(Input IN, inout SurfaceOutputStandard o) {
+				float4 col = float4(1, 1, 1, 1);
+				
+				o.Albedo = col.rgb;
+				o.Metallic = _Metallic;
+				o.Smoothness = _Glossiness;
+				o.Alpha = col.a;
+			}
+			ENDCG
+		}
+			FallBack "Diffuse"
+}
---
+++ Assets/SPH_Compute.cs
@@ -0,0 +1,126 @@
+using System.Runtime.InteropServices;
+using UnityEngine;
+using System.Collections.Generic;
+
+public class SPH_Compute : MonoBehaviour
+{
+    
+    [System.Serializable]
+    [StructLayout(LayoutKind.Sequential, Size=44)]
+    public struct Particle {
+        public float pressure; // 4
+        public float density; // 8
+        public Vector3 currentForce; // 20
+        public Vector3 velocity; // 32
+        public Vector3 position; // 44
+    }
+
+
+    [Header("General")]
+    public Vector3 boxSize;
+    public Vector3 spawnBoxCenter;
+    public Vector3 spawnBox;
+    public float particleRadius;
+    public Vector3 gravity = new Vector3(0, -9.81f, 0);
+
+    [Header("Fluid Constants")]
+    public float boundDamping = -0.5f;
+    public float viscosity = 200f;
+    public float particleMass = 2.5f;
+    public float gasConstant = 2000.0f; // Includes temp
+    public float restingDensity = 300.0f; // Water
+
+
+    [Header("Time")]
+    public float timeScale;
+
+    [Header("Compute")]
+    public ComputeShader shader;
+    private Particle[] particles;
+    
+    private ComputeBuffer _argsBuffer;
+    private ComputeBuffer _particlesBuffer;
+
+    [Header("Particle Rendering")]
+    public Mesh particleMesh;
+    public float particleRenderSize = 40f;
+    public Material material;
+
+    private int num = 0;
+
+    private static readonly int SizeProperty = Shader.PropertyToID("_size");
+    private static readonly int ParticlesBufferProperty = Shader.PropertyToID("_particlesBuffer");
+
+    private void Awake() {
+        // Spawn Particles
+        SpawnParticlesInBox();
+
+        uint[] args = {
+            particleMesh.GetIndexCount(0),
+            (uint) num,
+            particleMesh.GetIndexStart(0),
+            particleMesh.GetBaseVertex(0),
+            0
+        };
+        _argsBuffer = new ComputeBuffer(1, args.Length * sizeof(uint), ComputeBufferType.IndirectArguments);
+        _argsBuffer.SetData(args);
+
+        InitializeComputeBuffers();
+    }
+    
+    private void InitializeComputeBuffers() {
+        _particlesBuffer = new ComputeBuffer(num, 44);
+        _particlesBuffer.SetData(particles);
+    }
+
+
+    private void SpawnParticlesInBox() {
+        Vector3 spawnTopLeft = spawnBoxCenter - spawnBox / 2;
+        int xIterations = Mathf.RoundToInt(spawnBox.x / (particleRadius * 2));
+        int yIterations = Mathf.RoundToInt(spawnBox.y / (particleRadius * 2));
+        int zIterations = Mathf.RoundToInt(spawnBox.z / (particleRadius * 2));
+
+        num = xIterations * yIterations * zIterations;
+
+        List<Particle> _particles = new List<Particle>();
+
+        for (int x = 1; x < xIterations; x++) {
+            for (int y = 1; y < yIterations; y++) {
+                for (int z = 1; z < zIterations; z++) {
+
+                    Vector3 spawnPosition = spawnTopLeft + new Vector3(x * particleRadius * 2, y * particleRadius * 2, z * particleRadius * 2) + Random.onUnitSphere * particleRadius * 0.5f;
+
+                    Particle p = new Particle
+                    {
+                        position = spawnPosition
+                    };
+
+                    _particles.Add(p);
+                }
+            }
+        }
+
+        particles = _particles.ToArray();
+
+    }
+
+    private void Update() {
+
+        material.SetFloat(SizeProperty, particleRenderSize);
+        material.SetBuffer(ParticlesBufferProperty, _particlesBuffer);
+
+        Graphics.DrawMeshInstancedIndirect(particleMesh, 0, material, new Bounds(Vector3.zero, boxSize), _argsBuffer, castShadows: UnityEngine.Rendering.ShadowCastingMode.Off);
+    }
+
+    private void OnDrawGizmos() {
+        Gizmos.color = Color.blue;
+        Gizmos.DrawWireCube(Vector3.zero, boxSize);
+
+        if (!Application.isPlaying)
+        {
+            Gizmos.color = Color.cyan;
+            Gizmos.DrawWireCube(spawnBoxCenter, spawnBox);
+        }
+    }
+
+}
